2cbcc73b8cbb2f6a3f5fffc6a18019c3f1c8d066
Michael Steven Rincon Tellez <michael.rincon@javeriana.edu.co>
2020-09-29 19:45:51 -0500

    Pantalla login completada

--- Frontend Angular/src/app/login/login.component.ts
+++ Frontend Angular/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoginServicesService } from '../../services/login-services.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route: ActivatedRoute, private router: Router, private loginService: LoginServicesService) { }
+
+
+  user = '';
+  password = '';
+
 
   ngOnInit(): void {
+  }
+
+  submit(): void {//REALIZAR VALIDACIONES CAMPOS COMO EN LA OTRA PAGINA
+    console.log(this.user + ' - ' + this.password);
+    this.loginService.login(this.user, this.password).subscribe(data => {
+        this.router.navigate(['foros']);
+      }, error => {
+        console.error(error);
+      });
+  }
+
+  logout() {
+    this.loginService.logout().subscribe(data => {
+      }, error => {
+        console.error(error);
+      });
   }
 
 }
--- Frontend Angular/src/services/login-services.service.ts
+++ Frontend Angular/src/services/login-services.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginServicesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string) {
+    const formHeaders = new HttpHeaders();
+    formHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+    const formParams = new HttpParams()
+      .set('username', username)
+      .set('password', password);
+    return this.http.post('http://localhost:8080/login', null, {
+      headers: formHeaders,
+      params: formParams,
+      withCredentials: true
+    });
+  }
+
+  logout() {
+    return this.http.post('http://localhost:8080/logout', '', {
+      withCredentials: true
+    });
+  }
 }
